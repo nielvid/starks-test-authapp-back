2b11a886de475c7c4fa866dc4dc7407d83ba140b
nielvid <nielvidservices@gmail.com>
2021-01-23 11:41:24 +0100

    server.js and package.json updated

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ var mongoose = require('mongoose')
 const dotenv = require("dotenv");
 dotenv.config();
 
-mongoose.connect(process.env.DB_CONNECT , {useNewUrlParser: true,useUnifiedTopology: true})
+mongoose.connect(process.env.DB_CONNECT , {useNewUrlParser: true,useUnifiedTopology: true, useFindAndModify: false,  useCreateIndex: true,})
 .then((response)=>{
-	app.listen(process.env.PORT || 5000 )
-	console.log(`Server Started at port or 5000`)
+	
     console.log("Connected to database")
 	
 })
@@ -27,13 +26,21 @@ app.use(express.json()); // for body parser
 app.use (Route)
 
 // serve build if on production
-if(process.env.NODE_ENV =="producntion"){
+if(process.env.NODE_ENV === "production"){
+
+  //set static folder
   app.use(express.static('client/build'))
+
+  //get anything, load index.html
   app.get('*', (req, res)=>{
     res.sendFile(path.resolve(__dirname, 'client', 'build', index.html))
   })
 }
 
+
+app.listen(process.env.PORT || 5000, (req, res)=>{
+	console.log(`Server Started on port ${PORT} or 5000`)
+})
 
 
 
